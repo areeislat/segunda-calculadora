82ce24168c8c127609502f60a1e964f992d0f703
Areliz <are.isla@duocuc.cl>
2025-04-20 22:30:45 -0400

    implementacion botones

--- src/Calculator.java
+++ src/Calculator.java
@@ -39,7 +39,7 @@ public class Calculator {
     String B = null;
 
     Calculator(){
-        frame.setVisible(true);
+        //frame.setVisible(true);
         frame.setSize(boardWidth,boardHeight);
         frame.setLocationRelativeTo(null);
         //que no pueda cambiar el tamaño de la ventana
@@ -90,7 +90,37 @@ public class Calculator {
                 public void actionPerformed(ActionEvent e) {
                     JButton button = (JButton) e.getSource();
                     String buttonValue = button.getText();
-                    if (Arrays.asList(rightSymbols).contains(buttonValue)) {}
+                    if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+                        if (buttonValue == "="){
+                            if(A != null) {
+                                B = displayLabel.getText();
+                                double numA = Double.parseDouble(A);
+                                double numB = Double.parseDouble(B);
+
+                                if (operator ==  "+") {
+                                    displayLabel.setText(removeZeroDecimal(numA + numB));
+                                }
+                                else if (operator == "-") {
+                                    displayLabel.setText(removeZeroDecimal(numA - numB));
+                                }
+                                else if (operator == "x") {
+                                    displayLabel.setText(removeZeroDecimal(numA * numB));
+                                }
+                                else if (operator == "÷") {
+                                    displayLabel.setText(removeZeroDecimal(numA / numB));
+                                }
+                                clearAll();
+                            }
+                        }
+                        else if ("+-x÷".contains(buttonValue)) {
+                            if (operator == null) {
+                                A = displayLabel.getText();
+                                displayLabel.setText("0");
+                                B = "0";
+                            }
+                            operator = buttonValue;
+                        }
+                    }
                     else if (Arrays.asList(topSymbols).contains(buttonValue)) {
                         if (buttonValue == "AC") {
                             //limpia todo y devuelve el valor a 0
@@ -125,6 +155,7 @@ public class Calculator {
                 }
                 
             });
+            frame.setVisible(true);
         }
 
     }
